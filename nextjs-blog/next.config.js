8ec66ddf8c95c55324b53687a800b19759364c55
pyyding <kaspar@brainbase.com>
2021-06-10 14:05:35 +0300

    fixes

--- nextjs-blog/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.symlinks = false
-    // Important: return the modified config
-    return config
-  },
-}
